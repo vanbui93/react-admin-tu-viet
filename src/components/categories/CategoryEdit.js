b0c5d9b2fe5290532f722307d03e569a27271703
vanbui93 <van.bt.38@gmail.com>
2020-08-02 22:47:12 +0700

    update CategoryList

--- src/components/categories/CategoryEdit.js
+++ src/components/categories/CategoryEdit.js
@@ -10,12 +10,12 @@ import {
     TextInput,
 } from 'react-admin';
 
-// import ThumbnailField from '../products/ThumbnailField';
-// import ProductRefField from '../products/ProductRefField';
+import ThumbnailField from './../products/ThumbnailField';
+import ProductRefField from './../products/ProductRefField';
 
 const CategoryTitle = translate(({ record, translate }) => (
     <span>
-        {translate('resources.categories.name', { smart_count: 1 })} &quot;{
+        {translate('name', { smart_count: 1 })} &quot;{
             record.name
         }&quot;
     </span>
@@ -32,8 +32,8 @@ const CategoryEdit = props => (
                 perPage={5}
             >
                 <Datagrid>
-                    {/* <ThumbnailField />
-                    <ProductRefField source="reference" /> */}
+                    <ThumbnailField />
+                    <ProductRefField source="reference" />
                     <NumberField
                         source="price"
                         options={{ style: 'currency', currency: 'USD' }}
@@ -46,9 +46,10 @@ const CategoryEdit = props => (
                         source="height"
                         options={{ minimumFractionDigits: 2 }}
                     />
-                    <NumberField source="stock" />
-                    <EditButton />
+                    <NumberField source="stock" /> */}
+                <EditButton />
                 </Datagrid>
+           
             </ReferenceManyField>
         </SimpleForm>
     </Edit>
